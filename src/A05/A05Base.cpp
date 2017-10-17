5337a69b84f5c8389439e504ef1acd6436dc07da
oscar espejo <xxgreysoulxx@gmail.com>
2017-10-17 10:38:51 +0200

    text image

--- src/A05/A05Base.cpp
+++ src/A05/A05Base.cpp
@@ -1,5 +1,6 @@
 #include <SDL.h>		// Always needs to be included for an SDL app
 #include <SDL_image.h>  //Libreria que controla imagenes
+#include <SDL_ttf.h>	//libreria de texto
 
 //Game general information
 #define SCREEN_WIDTH 800
@@ -11,7 +12,9 @@ int main(int, char*[]) {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) throw "No es pot inicialitzar SDL subsystems";
 	//-------
 	const Uint8 imgFlags{ IMG_INIT_PNG | IMG_INIT_JPG };
-	if (!(IMG_Init(imgFlags) & imgFlags)) throw "Error: SDL_image init";
+	if (!(IMG_Init(imgFlags) & imgFlags)) throw "Error: SDL_image init";//image init
+	//-------
+	if (TTF_Init() != 0) throw "No es pot inicialitzar SDL_TTF";//texto init
 
 	// --- WINDOW ---
 	SDL_Window *window{ SDL_CreateWindow("SDL...", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN) };
@@ -22,13 +25,27 @@ int main(int, char*[]) {
 	if (renderer == nullptr) throw "No es pot inicialitzar SDL_Renderer";
 
 	// --- SPRITES ---
+	//Bg
 	SDL_Texture *bgTexture{ IMG_LoadTexture(renderer, "../../res/img/bg.jpg") };
 	if (bgTexture == nullptr) throw "No s'han pogut crear les textures";
 	SDL_Rect bgRect{ 0,0,SCREEN_WIDTH, SCREEN_HEIGHT };
+
+	//Player
+	SDL_Texture *playerTexture{ IMG_LoadTexture(renderer, "../../res/img/kintoun.png") };
+	if (playerTexture == nullptr) throw "No s'han pogut crear les textures";
+	SDL_Rect playerRect{ 0,0,350,189 };
+	SDL_Rect playerTarget{ 0,0,100,100 };//player target para hacerlo mas fino el movimiento
 		// --- Animated Sprite ---
 
 	// --- TEXT ---
-
+	TTF_Font *font{ TTF_OpenFont("../../res/ttf/saiyan.ttf", 80) };
+	if (font == nullptr) throw "No es pot initzializar";
+	SDL_Surface *tmpSurf{ TTF_RenderText_Blended(font, "My first sdl Game", SDL_Color{155,0,255,255}) };
+	if (tmpSurf == nullptr) TTF_CloseFont(font), throw "Unable to creat the SDL SURFACE";
+	SDL_Texture *textTexture{ SDL_CreateTextureFromSurface(renderer, tmpSurf) };
+	SDL_Rect textRect{ 100,50,tmpSurf->w, tmpSurf->h };//witdh and height 
+	SDL_FreeSurface(tmpSurf);
+	TTF_CloseFont(font);
 	// --- AUDIO ---
 
 	// --- GAME LOOP ---
@@ -40,17 +57,22 @@ int main(int, char*[]) {
 			switch (event.type) {
 			case SDL_QUIT:		isRunning = false; break;
 			case SDL_KEYDOWN:	if (event.key.keysym.sym == SDLK_ESCAPE) isRunning = false; break;
+			case SDL_MOUSEMOTION:playerRect.x = event.motion.x-50; playerRect.y = event.motion.y-50; break;//evento de movimiento del player
 			default:;
 			}
 		}
 
 		// UPDATE
+		playerRect.x += (playerTarget.x - playerRect.x) / 10;
+		playerRect.y += (playerTarget.y - playerRect.y) / 10;
 
 		// DRAW
 			//Background
 		SDL_RenderClear(renderer);//limpia pantalla SIEMPRE AL PRINCIPIO DEL DRAW
 		SDL_RenderCopy(renderer, bgTexture, nullptr, &bgRect); // aqui dibujamos la textura usando un rectangulo - IMPORTANTE -
-		SDL_RenderPresent(renderer);
+		SDL_RenderCopy(renderer, playerTexture, nullptr, &playerRect);//dibujamos la textura del nuvol quinton
+		SDL_RenderCopy(renderer, textTexture, nullptr, &textRect);
+		SDL_RenderPresent(renderer);//solo 1
 			//Animated Sprite
 		SDL_RenderPresent(renderer);
 
@@ -58,11 +80,14 @@ int main(int, char*[]) {
 
 	// --- DESTROY ---
 	SDL_DestroyTexture(bgTexture);//destruye el bg
+	SDL_DestroyTexture(playerTexture);//destuye al player
+	SDL_DestroyTexture(textTexture);//destruyo el texto
 	SDL_DestroyRenderer(renderer);//primero destruye al render por que esta referenciado por el window
 	SDL_DestroyWindow(window);
 
 	// --- QUIT ---
-	SDL_Quit();
 	IMG_Quit(); //Quit de las imagenes
+	TTF_Quit();
+	SDL_Quit();//SIEMPRE VA ULTIMO
 	return 0;
 }
